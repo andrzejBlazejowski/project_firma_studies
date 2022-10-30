cf0855f2843a15cfd7ae5087cd7c48adc593739e
andrzejBlazejowski <andrzej.blazejowski@gmail.com>
2022-10-30 09:46:12 +0100

    Add project files.

---
+++ Firma/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,165 @@
+﻿using Firma.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace Firma.ViewModels
+{
+    public  class MainWindowViewModel : BaseViewModel
+    {
+        //będzie zawierała kolekcje komend, które pojawiają się w menu lewym oraz kolekcje zakładek 
+        #region Komendy menu i paska narzedzi
+        public ICommand NowyTowarCommand //ta komenda zostanie podpieta pod menu i pasek narzedzi
+        { 
+            get
+            {
+                return new BaseCommand(()=>createView(new NowyTowarViewModel()));//to jest komenda, która wyw. funkcję createTowar
+            }
+        }
+        public ICommand TowaryCommand
+        {
+            get
+            {
+                return new BaseCommand(showAllTowar);
+            }
+        }
+        public ICommand NowaFakturaCommand 
+        {
+            get
+            {
+                return new BaseCommand(() => createView(new NowaFakturaViewModel()));
+            }
+        }
+        public ICommand FakturyCommand
+        {
+            get
+            {
+                return new BaseCommand(showAllFaktury);
+            }
+        }
+        #endregion
+
+        #region Przyciski w menu z lewej strony
+        private ReadOnlyCollection<CommandViewModel> _Commands;//to jest kolekcja komend w emnu lewym
+        public ReadOnlyCollection<CommandViewModel> Commands
+        {
+            get 
+            { 
+                if(_Commands == null)//sprawdzam czy przyciski z lewej strony menu nie zostały zainicjalizowane
+                {
+                    List<CommandViewModel> cmds = this.CreateCommands();//tworzę listę przyciskow za pomocą funkcji CreateCommands
+                    _Commands = new ReadOnlyCollection<CommandViewModel>(cmds);//tę listę przypisuje do ReadOnlyCollection (bo readOnlyCollection można tylko tworzyć, nie można do niej dodawać)
+                }
+                return _Commands; 
+            }   
+        }
+        private List<CommandViewModel> CreateCommands()//tu decydujemy jakie przyciski są w lewym menu
+        {
+            return new List<CommandViewModel>
+            {
+                new CommandViewModel("Towary",new BaseCommand(showAllTowar)), //to tworzy pierwszy przycisk o nazwie Towary, który pokaże zakładkę wszystkie towary
+                new CommandViewModel("Towar",new BaseCommand(()=>createView(new NowyTowarViewModel()))),
+                new CommandViewModel("Katura",new BaseCommand(()=>createView(new NowaFakturaViewModel()))),
+                new CommandViewModel("Faktury",new BaseCommand(showAllFaktury)),
+            };
+        }
+        #endregion
+
+        #region Zakładki
+        private ObservableCollection<WorkspaceViewModel> _Workspaces; //to jest kolekcja zakładek
+        public ObservableCollection<WorkspaceViewModel> Workspaces
+        {
+            get 
+            {
+                if(_Workspaces == null)
+                {
+                    _Workspaces = new ObservableCollection<WorkspaceViewModel>();
+                    _Workspaces.CollectionChanged += this.onWorkspacesChanged;
+                }
+                return _Workspaces;
+            }
+        }
+        private void onWorkspacesChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.NewItems != null && e.NewItems.Count != 0)
+                foreach (WorkspaceViewModel workspace in e.NewItems)
+                    workspace.RequestClose += this.onWorkspaceRequestClose;
+
+            if (e.OldItems != null && e.OldItems.Count != 0)
+                foreach (WorkspaceViewModel workspace in e.OldItems)
+                    workspace.RequestClose -= this.onWorkspaceRequestClose;
+        }
+        private void onWorkspaceRequestClose(object sender, EventArgs e)
+        {
+            WorkspaceViewModel workspace = sender as WorkspaceViewModel;
+            //workspace.Dispos();
+            this.Workspaces.Remove(workspace);
+        }
+        #endregion
+        #region Funkcje pomocnicze
+        private void createView(WorkspaceViewModel workspace)
+        {
+            this.Workspaces.Add(workspace);
+            this.setActiveWorkspace(workspace);
+        }
+        private void showAllFaktury()
+        {
+            WszystkieFakturyViewModel workspace = this.Workspaces.FirstOrDefault(vm => vm is WszystkieFakturyViewModel) as WszystkieFakturyViewModel;
+            if (workspace == null)
+            {
+                workspace = new WszystkieFakturyViewModel();
+                this.Workspaces.Add(workspace);
+            }
+            this.setActiveWorkspace(workspace);
+        }
+        //to jest funkcja, która otwiera nową zakładke Towar
+        //za każdym tworzy nową NOWĄ zakładkę do dodawania towaru
+        //private void createTowar()
+        //{
+        //    //tworzy zakładke NowyTowar (VM)
+        //    NowyTowarViewModel workspace=new NowyTowarViewModel();
+        //    //dodajmy ją do kolkcji aktywnych zakładek
+        //    this.Workspaces.Add(workspace);
+        //    this.setActiveWorkspace(workspace);
+        //}
+        //to jest funkcja, która otwiera zakładke ze wszystkimi towarami
+        //za każdym razem sprawdza czy zakladka z towarami jest juz otwarta, jak jest to ja aktywuje, ajk nie ma to tworzy 
+        private void showAllTowar()
+        {
+            //sz....
+            WszystkieTowaryViewModel workspace = this.Workspaces.FirstOrDefault(vm => vm is WszystkieTowaryViewModel) as WszystkieTowaryViewModel;
+            //jezeli ....
+            if(workspace == null)
+            {
+                //tworzymy nowa zakladke Wszystkie towary
+                workspace=new WszystkieTowaryViewModel();
+                //i dodajemy ja do kolekcji zakladek
+                this.Workspaces.Add(workspace);
+            }
+            //aktywujemy zakladke
+            this.setActiveWorkspace(workspace);
+        }
+        private void setActiveWorkspace(WorkspaceViewModel workspace)
+        {
+            Debug.Assert(this.Workspaces.Contains(workspace));
+
+            ICollectionView collectionView = CollectionViewSource.GetDefaultView(this.Workspaces);
+            if (collectionView != null)
+                collectionView.MoveCurrentTo(workspace);
+        }
+
+
+        #endregion
+
+
+
+    }
+}
